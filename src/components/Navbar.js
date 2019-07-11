5b798ed8e6aa497751bde32e52288dc44efa2d46
Fernando Augusto <fernandoajneto@gmail.com>
2019-07-11 15:55:24 -0300

    Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <div className='navbar__container'>
+        <span>Logo</span>
+        <span>Sair</span>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
