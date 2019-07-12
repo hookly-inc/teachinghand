98317b7f5998c6b77546299d5d25ba077cb551bc
Fernando Augusto <fernandoajneto@gmail.com>
2019-07-12 20:57:07 -0300

    Progress bar

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ProgressBar extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Progresso: </h3>
+        <div class="progress-bar"></div>
+      </div>
+    );
+  }
+}
+
+export default ProgressBar;
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import ProgressBar from '../../components/ProgressBar';
 
 const Dashboard = () => {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <div className="dashboard">
+        <ProgressBar />
+      </div>
+    </div>
   )
 }
 
