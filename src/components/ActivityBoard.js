943906a17956364b0e62473dc673a01540a8ea5e
Fernando Augusto <fernandoajneto@gmail.com>
2019-07-15 14:11:26 -0300

    Modal & Video

--- src/components/ActivityBoard.js
+++ src/components/ActivityBoard.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
+import Video from './Video';
 
 class ActivityBoard extends Component {
+  state = {
+    showVideo: false
+  }
+
+  handleVideo() {
+    this.setState({
+      showVideo: !this.state.showVideo
+    })
+  }
+
   render() {
     return (
       <div className='activity-board'>
-        <div className='activity-board__photo'></div>
+        <div className='activity-board__photo' onClick={() => this.handleVideo()}></div>
         <div className='activity-board__info'></div>
+
+        <Modal isOpen={this.state.showVideo} className='activity-board__video'
+        onRequestClose={() => this.handleVideo()}
+        closeTimeoutMS={200}>
+          <Video code='nuYOPO4MAzM'/>
+        </Modal>
       </div>
     )
   }
