23a8a48d62a358bed5e1d2b5bf4908ae7343fdd1
Fernando Augusto <fernandoajneto@gmail.com>
2019-07-22 00:56:39 -0300

    Login screen

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import macaco from '../../images/macaquinho.png';
 
 const Login = () => {
   return (
     <div className="login">
-      <div className="login__container">
-        <h1>Página de login</h1>
-        <Link to='/dashboard'>Entrar </Link> | 
-        <Link to='/register'> Cadastrar-se</Link>
+      <img src={macaco} className="login-logo" alt="Teaching Hand"/>
+      <div className="login-container">
+        <h1 className="login-container__header">Seja bem vindo!</h1>
+
+        <div className="login-form">
+          <form>
+            <div className="login-form__group">
+              <input type="email" className="login-form__input" placeholder="E-mail"/>
+            </div>
+            <div className="login-form__group">
+              <input type="password" className="login-form__input" placeholder="Senha"/>
+            </div> 
+            <div className="login-form__group">
+              <input type="checkbox" className="login-form__check"/> 
+              <label> Manter conectado</label>
+            </div>
+            <Link className="login-form-button login-form-button__primary" to='/dashboard'>Entrar </Link> 
+            <Link className="login-form-button login-form-button__secondary" to='/register'> Cadastrar-se</Link>
+          </form>
+        </div>
       </div>
     </div>
   )
